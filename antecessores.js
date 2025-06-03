9373cb15e9d88f1fc9c0cca83eb3d6d3c842483d
iamlagr <lucasandersong2021@gmail.com>
2025-06-03 16:42:10 -0300

    Add files via upload

---
+++ antecessores.js
@@ -0,0 +1,4 @@
+let num = Number(prompt("digite um numero: "))
+for(; num >=1; num--){
+    console.log(num+", ")}
+    
---
+++ fatorial.js
@@ -0,0 +1,6 @@
+let num = Number(prompt("digite um numero: "))
+let fatorial = 1 
+for ( ; num >=1; num--){
+   fatorial = fatorial * num
+}
+alert(fatorial)
