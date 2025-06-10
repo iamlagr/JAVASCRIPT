1530df3340562eddfbc03be25061e91af192ad22
iamlagr <lucasandersong2021@gmail.com>
2025-06-10 16:27:58 -0300

    Add files via upload

---
+++ constante array vetores (variavel nova).js
@@ -0,0 +1,10 @@
+const frutas = ["uva"," laranja", "tangerina"]
+console.log(frutas)
+
+ function buscarporposicao() {
+    posicao=prompt("Escolha elemento do array")
+    alert(frutas[posicao])
+ }
+function tamanhodoarray(){
+alert(frutas.length)
+}
